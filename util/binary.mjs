efbed6faac61fb86e80bdb3d2aaab8b0e0698e45
techno-express <technoexpressnet@gmail.com>
2020-11-29 23:13:05 -0500

    check/test with 7za, getting remote build fails, when locally everything fine7z

--- util/binary.mjs
+++ util/binary.mjs
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(
   import.meta.url);
 const __dirname = dirname(__filename);
 
-export default function (override = false, useBinary = '7z') {
+export default function (override = false, useBinary = '7za') {
   let binaryPath = join(__dirname, '..', "binaries", (override == true ? process.platform + sep + 'other32' : process.platform));
   let binaryFilename = (process.platform == "win32") ? useBinary + '.exe' : useBinary;
   return {
