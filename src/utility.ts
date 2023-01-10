bd3dce680a7557a0e361b6062d2d3f0ec1dbbab4
Rashid Lazytech <lazyt3ch@gmail.com>
2023-01-10 06:11:00 +0500

    Revert "make Files function more compact (utility.ts); install dependencies"
    This reverts commit f3adee703532bf9cd3dda02f51a1e94c07521eca.

--- src/utility.ts
+++ src/utility.ts
@@ -34,10 +34,22 @@ export const Binary = function (override = false, binary = '7z') {
  * @return {string}
  */
 export const Files = function (files: string | string[]): string {
-    if (isUndefined(files)) return '';
-    return (Array.isArray(files) ? files : [files])
-        .map((file) => `"${file}"`)
-        .join(' ');
+    if (isUndefined(files)) {
+        return '';
+    }
+
+    let toProcess = '';
+
+    if (Array.isArray(files)) {
+        files.forEach(function (f) {
+            toProcess += '"' + f + '" ';
+        });
+        toProcess = toProcess.trim();
+    } else {
+        toProcess = '"' + files + '"';
+    }
+
+    return toProcess;
 };
 
 /**
