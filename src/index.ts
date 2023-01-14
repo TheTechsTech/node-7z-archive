7e87f4ba267c78af955a396035876e15b3dba37e
Rashid Lazytech <lazyt3ch@gmail.com>
2023-01-14 09:20:40 +0500

    refactor deleteArchive function (index.ts)

--- src/index.ts
+++ src/index.ts
@@ -117,24 +117,20 @@ export const deleteArchive =
                 // Convert array of files into a string if needed.
                 files = Files(files);
                 // Create a string that can be parsed by `run`.
-                let command = 'd "' + filepath + '" ' + files;
+                let command = `d "${filepath}" ${files}`;
                 // Start the command
-                Run('7z', command, options, override) // When all is done resolve the Promise.
-                    .then(function (args) {
-                        return resolve(args);
-                    }) // Catch the error and pass it to the reject function of the Promise.
-                    .catch(function () {
-                        console.error(
-                            'DeleteArchive failed using `7z`, retying with `7za`.'
-                        );
-                        Run('7za', command, options, override)
-                            .then(function (args) {
-                                return resolve(args);
-                            })
-                            .catch(function (err) {
-                                return reject(err);
-                            });
+                const executables = ['7z', '7za']; // Two or more items
+                let position = 0;
+                const runner = () => Run(executables[position], command, options, override)
+                    .then((args: any[]) => resolve(args)) // When all is done resolve the Promise.        
+                    .catch((err: any) => { // Catch the error and pass it to the reject function of the Promise.
+                        if (position === executables.length - 1) return reject(err);
+                        console.error('DeleteArchive failed using `' + executables[position] + 
+                            '`, retrying with `' + executables[position + 1] + '`.');
+                        position++;
+                        runner();
                     });
+                return runner();
             });
         });
 
